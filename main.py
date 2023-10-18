6d818b61a32c8401ab76040171c546da1757b644
Dohodyaga <108049859+TTGTTE@users.noreply.github.com>
2023-10-18 15:55:40 +0500

    Update main.py
    Больше комментов

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from keras.callbacks import EarlyStopping
+import tkinter as tk
+from tkinter import filedialog
+
 
 # Функция для загрузки данных и извлечения аудиопризнаков
 def load_data(folder):
@@ -21,21 +24,29 @@ def load_data(folder):
             for filename in os.listdir(class_folder):
                 if filename.endswith(".wav"):
                     audio_path = os.path.join(class_folder, filename)
-                    
+
                     # Загрузка аудиофайла с частотой дискретизации 44100 и длительностью 3 секунды
+                    # Немного сатисфайсинг, необходимо не хардкодить данные значения, либроза тянется
                     audio, _ = librosa.load(audio_path, sr=44100, duration=3.0)
 
                     # Извлечение MFCC (Mel-frequency cepstral coefficients) и Chroma признаков
+                    # Входной аудиосигнал можно было сделать и получше, слишком ресурсоёмкий,
+                    # если датасет сделан не для обучения
                     mfccs = librosa.feature.mfcc(y=audio, sr=44100, n_mfcc=13)
                     chroma = librosa.feature.chroma_stft(y=audio, sr=44100)
 
                     # Объединение MFCC и Chroma в один массив признаков
+                    # Лучше использовать преаллокацию массивов,
+                    # но возникает Гидра
                     features = np.vstack((mfccs, chroma))
-                    features = np.expand_dims(features, axis=-1)  # Расширение размерности для соответствия формату входных данных модели
-                    X.append(features)
+                    features = np.expand_dims(features, axis=-1)    # Расширение размерности для 
+                    X.append(features)                              # соответствия формату входных данных модели
+
                     labels.append(class_name)
 
     # Преобразование меток в бинарный формат
+    # Боюсь, что при огромных количествах классов
+    # начнутся проблемы с памятью
     mlb = MultiLabelBinarizer()
     y = mlb.fit_transform([[label] for label in labels])
 
@@ -43,10 +54,20 @@ def load_data(folder):
     np.save('classes.npy', mlb.classes_)
     return np.array(X), np.array(y)
 
+
 # Функция для создания модели нейронной сети
 def create_model(input_shape, num_labels):
+
     # Инициализация модели
     model = tf.keras.Sequential([
+
+        # Пару слов об интерпретации и анализа
+        # Условно и в теории возможно
+        # Просто надеюсь, что этим буду заниматься не я
+
+        # Важно!!!
+        # Модель слегка неоптимизированна
+        # Но это из-за архитектуры, отвечаю
 
         # Два последовательных LSTM слоя
         tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(input_shape[0], input_shape[1])),
@@ -54,15 +75,17 @@ def create_model(input_shape, num_labels):
 
         # Полносвязные слои
         tf.keras.layers.Dense(256, activation='relu'),
-        tf.keras.layers.Dropout(0.3),  # Слой Dropout для регуляризации
-        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.3),                       # Слой Dropout для регуляризации
+        tf.keras.layers.Dense(128, activation='relu'),      # Всё равно есть риск переобучения
 
         # Выходной слой с сигмоидной активацией для многоклассовой классификации
         tf.keras.layers.Dense(num_labels, activation='sigmoid')
     ])
+
     # Компиляция модели с оптимизатором Adam и функцией потерь binary_crossentropy
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
+
 
 # Функция для классификации аудиофайла
 def classify_audio(model, mlb, audio_path):
@@ -78,13 +101,16 @@ def classify_audio(model, mlb, audio_path):
     predictions = model.predict(features)
 
     # Пороговое значение для классификации
+    # Подбор значения вероятности ещё не закончен
+    # Надо играть с цифрами
     thresholded_output = (predictions >= 0.5).astype(np.int64)
 
     # Инвертирование бинарных меток в имена классов
     predicted_labels = mlb.inverse_transform(thresholded_output)[0]
     return predicted_labels
 
-# Главная функция для управления всем процессом
+
+
 def main():
     model = None                    # Инициализация модели
     mlb = MultiLabelBinarizer()     # Инициализация бинаризатора меток
@@ -92,10 +118,11 @@ def main():
     # Загрузка сохраненных классов, если они существуют
     if os.path.exists('classes.npy'):
         mlb.classes_ = np.load('classes.npy', allow_pickle=True)
-
+    
     # Инициализация ранней остановки для предотвращения переобучения
     early_stopping = EarlyStopping(monitor='val_loss', patience=10)
 
+    
     while True:
         print("Выберите действие:")
         print("1. Обучить нейронную сеть")
@@ -107,35 +134,55 @@ def main():
 
         # Обучение модели
         if choice == '1':
-            X, y = load_data("Dataset")                     # Загрузка данных
-            X_val, y_val = load_data("Validation")          # Загрузка валидационных данных
-            model = create_model(X[0].shape, y.shape[1])    # Создание модели
+            X, y = load_data("Dataset")             # Загрузка данных 
+            X_val, y_val = load_data("Validation")  # Загрузка валидационных данных
+            
+            # Что это вообще такое?
+            # Легаси строки для дебага, сынок
+            # print(f"Форма X до вставки: {X.shape}")
+            # print(f"Форма X_val до вставки: {X_val.shape}")
+
+            # Создание модели
+            model = create_model(X[0].shape, y.shape[1])
 
             # Обучение модели с ранней остановкой и валидационным набором данных
             model.fit(X, y, epochs=1000, validation_data=(X_val, y_val), callbacks=[early_stopping])
-
+        
         # Тестирование модели
+        # Пару слов о тестированнии
+        # Если потеря больше, чем прирост точности,
+        # (или наоборот)
+        # то выводимая точность бывает +-5% от заявленной
         elif choice == '2':
-            X_test, y_test = load_data("Testing")               # Загрузка тестовых данных
-            loss, accuracy = model.evaluate(X_test, y_test)     # Оценка модели
-            print(f"Тестовая потеря: {loss}, Тестовая точность: {accuracy}")
+            if model is None:
+                print("Модель не обучена.")
+                continue
+            X_test, y_test = load_data("Validation")
+            loss, acc = model.evaluate(X_test, y_test)
+            print(f"Точность модели: {acc * 100}%")
 
         # Экспорт модели
         elif choice == '3':
-            model.save("trained_model.h5")  # Сохранение модели
+                model.save("FinalWork/model.h5")
 
         # Импорт модели
         elif choice == '4':
-            model = tf.keras.models.load_model("trained_model.h5")  # Загрузка модели
-
+            if os.path.exists("FinalWork/model.h5"):
+                model = tf.keras.models.load_model("FinalWork/model.h5")
+            else:
+                print("Файл модели не найден.")
+        
         # Классификация аудиофайла
         elif choice == '5':
+            if model is None:
+                print("Модель не обучена.")
+                continue
             audio_path = input("Введите путь к аудиофайлу: ")
-            labels = classify_audio(model, mlb, audio_path)  # Получение меток
-            print(f"Предсказанные метки: {labels}")
-
-        else:
-            print("Неизвестный выбор. Пожалуйста, попробуйте еще раз.")
+            if os.path.exists(audio_path) and audio_path.endswith('.wav'):
+                result = classify_audio(model, mlb, audio_path) # Получение меток
+                print(f"Музыкальные инструменты: {result}")
+            else:
+                print("Неверный путь к аудиофайлу. Пожалуйста, попробуйте еще раз.")
 
 if __name__ == "__main__":
     main()
